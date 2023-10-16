49b95d7e1221ed8ba6bc19f7deea874b916b5458
Genesis Cruz <genesis.cruzasturias@gmail.com>
2023-10-16 12:29:42 -0500

    commit

---
+++ src/ContactsManagerMain/Contacts.java
@@ -0,0 +1,28 @@
+package ContactsManagerMain;
+
+public class Contacts {
+
+    String name;
+    int phoneNumber;
+
+    public Contacts(String name, int phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
